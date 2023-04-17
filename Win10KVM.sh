c14f5fe8ff41ca535a275f21df6bbf6e580d6e94
EfeMethod1 <131073656+EfeMethod1@users.noreply.github.com>
2023-04-18 02:28:14 +0300

    Create Win10KVM.sh

---
+++ Win10KVM.sh
@@ -0,0 +1,10 @@
+echo Installing qemu
+sudo apt update && sudo apt install qemu-system-x86-64 neofetch -y
+clear
+echo Getting ISO File
+wget https://ss2.softlay.com/files/en_windows_10_22h2_x64_dvd.iso
+clear
+Creating Win10KVM img..
+qemu-img create -f qcow2 Win10KVM.img 90G
+echo Done! Booting Right now.
+sudo qemu-system-x86_64 -m 6G -cpu host --enable-kvm -vnc :0 -cdrom en_windows_10_22h2_x64_dvd.iso -boot d -hda Win10KVM.img

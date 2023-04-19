debf9e0dd1909bbcf93e28ca5c7971f9e6a79c7e
EfeMethod1 <131073656+EfeMethod1@users.noreply.github.com>
2023-04-20 00:42:18 +0300

    Update tcp_open.sh

--- tcp_open.sh
+++ tcp_open.sh
@@ -1,6 +1,3 @@
 echo Installing Ngrok for TCP
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
-echo Adding Token
-ngrok authtoken (your auth token)
-echo Creating TCP
-ngrok tcp 5900
+echo For Ngrok AuthToken, please type "ngrok authtoken (your token)" and type "ngrok tcp 5900".

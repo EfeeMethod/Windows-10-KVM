79cb8b09103766de3a383ed05b0e2a9926ca8b39
EfeeMethod <131386664+EfeeMethod@users.noreply.github.com>
2023-04-21 04:33:52 +0700

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
-sudo qemu-system-x86_64 -m 4G -cpu host --enable-kvm -vnc :0 -cdrom en_windows_10_22h2_x64_dvd.iso -hda Win10KVM.img -smp cores=3,sockets=1
+sudo qemu-system-x86_64 -m 4G -cpu host --enable-kvm -vnc :0 -cdrom en_windows_10_22h2_x64_dvd.iso -boot d -hda Win10KVM.img -smp cores=3,sockets=1
 sleep 43200

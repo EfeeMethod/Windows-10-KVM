f153aac5ed327c8aea561974b5c1ddefea908e00
EfeeMethod <131386664+EfeeMethod@users.noreply.github.com>
2023-04-21 02:24:09 +0700

    Update Win10KVM.sh

--- Win10KVM.sh
+++ Win10KVM.sh
@@ -7,5 +7,5 @@ clear
 Creating Win10KVM img..
 qemu-img create -f qcow2 Win10KVM.img 40G
 echo Done! Booting Right now.
-sudo qemu-system-x86_64 -m 4G -cpu host --enable-kvm -vnc :0 -cdrom en_windows_10_22h2_x64_dvd.iso -boot d -hda Win10KVM.img -smp cores=3,sockets=1
+sudo qemu-system-x86_64 -m 4G -cpu host --enable-kvm -vnc :0 -hda Win10KVM.img -smp cores=3,sockets=1
 sleep 43200
